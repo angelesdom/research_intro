c43f54148b9109ac1e7764b9c8b3ced855b8f9ab
Luis Dominguez <74793985+angelesdom@users.noreply.github.com>
2021-02-03 20:30:03 -0700

    Update fft_1.py

--- fft_1.py
+++ fft_1.py
@@ -5,18 +5,19 @@ from scipy import signal
 ###################TIME/MOMENTUM DOMAIN########################
 p0 = 2
 A = np.sqrt(1/(2*p0))
-t = np.linspace(-5,5,10000)
+t = np.linspace(0,10000,1000)
 y = np.zeros_like(t)
 y[(t > -2)*(t<2)] = A
 sample_signal = signal.square(2 * np.pi * 5 * t)
 
 #################Frequency/Position Domain#####################
 def plot_magnitude_spectrum(signal,title,f_ratio=1):
-    fft_val = fft(signal)
+    fft_val = fft(fftshift(signal))
     magnitude_spectrum = np.abs(fft_val)
     num_frequncy_bins = int(len(t) * f_ratio)
     plt.plot(t[:num_frequncy_bins],magnitude_spectrum[:num_frequncy_bins],label = title)
     plt.title(title)
+    plt.xlabel("Frequency (Hz)")
     plt.show()
 
-plot_magnitude_spectrum(sample_signal,"square wave",.01)
+plot_magnitude_spectrum(sample_signal,"square wave")
